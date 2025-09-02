fa2a37a9cfcd706b3140c855b9e91fde38c86ad9
Oscar Calle <oscarrcs123@gmail.com>
2025-09-02 08:37:59 -0500

    feat: replace User model with Student and OriginSchool models, update database provider to SQLite

---
+++ prisma/migrations/20250902133722_first/migration.sql
@@ -0,0 +1,60 @@
+-- CreateTable
+CREATE TABLE "Student" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "dni" TEXT NOT NULL,
+    "names" TEXT NOT NULL,
+    "surnames" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "originSchoolId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Student_originSchoolId_fkey" FOREIGN KEY ("originSchoolId") REFERENCES "OriginSchool" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "OriginSchool" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "CourseOffered" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "originalCapacity" INTEGER NOT NULL,
+    "currentCapacity" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Enrollment" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "status" TEXT NOT NULL DEFAULT 'PENDING',
+    "enrollmentFee" DECIMAL NOT NULL,
+    "createdAt" DATETIME NOT NULL,
+    "updatedAt" DATETIME NOT NULL,
+    "studentId" TEXT NOT NULL,
+    "courseOfferedId" TEXT NOT NULL,
+    CONSTRAINT "Enrollment_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "Student" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Enrollment_courseOfferedId_fkey" FOREIGN KEY ("courseOfferedId") REFERENCES "CourseOffered" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_dni_key" ON "Student"("dni");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_email_key" ON "Student"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "OriginSchool_name_key" ON "OriginSchool"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CourseOffered_name_key" ON "CourseOffered"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Enrollment_studentId_courseOfferedId_key" ON "Enrollment"("studentId", "courseOfferedId");
